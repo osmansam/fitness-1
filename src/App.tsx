d396e56a2bae0c888615a5bdb2c664f7a5f2c4a9
Osman <114867365+osmansam@users.noreply.github.com>
2023-04-17 17:24:10 -0400

    all done

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,12 @@ import { RootState, useAppDispatch } from "./store";
 import { setSelectPage, setIsTopOfPage } from "./features/context/contextSlice";
 import Navbar from "./scenes/navbar";
 import Home from "./scenes/home";
+import Benefit from "./scenes/benefits";
+import OurClasses from "./scenes/ourClasses";
+import ContactUs from "./scenes/contactUs";
+import Footer from "./scenes/footer";
 import { SelectedPage } from "./shared/types";
+
 function App() {
   const dispatch = useAppDispatch();
 
@@ -23,6 +28,10 @@ function App() {
     <div className="app bg-gray-20">
       <Navbar />
       <Home />
+      <Benefit />
+      <OurClasses />
+      <ContactUs />
+      <Footer />
     </div>
   );
 }
--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -35,7 +35,7 @@ const Navbar = () => {
   return (
     <nav>
       <div
-        className={`${navbarBackground} ${flexBetween} fixed top-0 z-30 w-full py-6`}
+        className={`${navbarBackground} ${flexBetween} fixed top-0 z-40 w-full py-6`}
       >
         <div className={`${flexBetween} mx-auto w-5/6`}>
           <div className={`${flexBetween} w-full gap-16`}>
